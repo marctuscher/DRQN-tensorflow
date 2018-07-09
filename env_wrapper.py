ec2d0736d1e3c2780c31a8f7b4cab80af765968a
Marc Tuscher <marc.tuscher@gmail.com>
2018-07-09 14:19:51 +0200

    deleted import of retro

--- env_wrapper.py
+++ env_wrapper.py
@@ -1,7 +1,6 @@
 import gym
 from utils import resize, rgb2gray
 import numpy as np
-import retro
 
 class GymWrapper():
 
@@ -61,52 +60,3 @@ class GymWrapper():
         return self.info['ale.lives']
 
 
-class RetroWrapper():
-
-    def __init__(self, env_name, state,screen_width, screen_height):
-        self.env = retro.make(game=env_name, state=state)
-        self.screen_width, self.screen_height = screen_width, screen_height
-        self._screen = None
-        self.reward = 0
-        self.terminal = True
-        self.info = {'ale.lives': 0}
-        self.random_start = 10
-        self.action_repeat = 1
-
-    def new_game(self):
-        if self.lives == 0:
-            self._screen = self.env.reset()
-        self._step(0)
-        return self.screen, 0, 0, self.terminal
-
-    def new_random_game(self):
-        self.new_game()
-        for _ in range(np.random.randint(0, self.random_start)):
-            self._step(0)
-        return self.screen, 0, 0, self.terminal
-
-    def _step(self, action):
-        self._screen, self.reward, self.terminal, self.info = self.env.step(action)
-
-    def random_step(self):
-        return self.env.action_space.sample()
-
-    def act(self, action):
-        cumulated = 0
-        start_lives = self.lives
-        for _ in range(self.action_repeat):
-            self._step(action)
-            cumulated = cumulated +self.reward
-            if start_lives > self.lives:
-                self.terminal = True
-            if self.terminal:
-                break
-        self.reward = cumulated
-
-
-    def screen(self):
-        return resize(rgb2gray(self._screen), (self.screen_height, self.screen_width))
-
-    @property
-    def lives(self):
-        return self.info['ale.lives']
